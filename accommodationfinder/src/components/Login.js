3b1237df4080f7427416ef8303effb05cc0bea10
lehanh19 <52921841+lehanh19@users.noreply.github.com>
2020-11-25 11:19:36 +0700

    Update Login

--- accommodationfinder/src/components/Login.js
+++ accommodationfinder/src/components/Login.js
@@ -2,7 +2,6 @@ import react, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
 import reactDOM from 'react-dom';
 import "../css/login.css";
-import "../css/unikit.css"
 import logo_trang from '../image/logo_ngang_trang.png';
 
 
@@ -17,44 +16,44 @@ class Login extends Component {
 
     render(){
         return(
-            <div className="uk-height-1-1">
-                <div className="uk-background-cover uk-background-center-center uk-height-1-1 ev-section-padding-v-s uk-panel uk-flex uk-flex-wrap uk-flex-middle uk-flex-center uk-light">
-                    <div className="uk-overlay ev-overlay-secondary uk-position-cover"></div>
+            <div className="login-height-1-1">
+                <div className="login-background-cover login-height-1-1 login-flex login-light">
+                    <div className="login-overlay-secondary login-position-cover"></div>
 
-                    <div className="ev-auth-2 uk-position-z-index">
-                        <NavLink  activeStyle={{color:'#fff'}} to="/home"><img className="ev-logo" src={logo_trang}/></NavLink>
+                    <div className="login-auth-2 login-position-z-index">
+                        <NavLink  activeStyle={{color:'#fff'}} to="/home"><img className="login-logo" src={logo_trang}/></NavLink>
                     
-                        <h5 className="uk-heading-line"><span>Đăng nhập</span></h5>
+                        <h5 className="login-heading-line"><span>Đăng nhập</span></h5>
 
                         <form>
-                            <div className="uk-margin">
-                                <div className="uk-width-1-1 uk-inline">
-                                    <input className="uk-input uk-border-pill" placeholder="Email" type="text" required/>
+                            <div className="login-margin">
+                                <div className="login-inline">
+                                    <input className="login-input login-border-pill" placeholder="Email" type="text" required/>
                                 </div>
                             </div>
-                            <div className="uk-margin">
-                                <div className="uk-width-1-1 uk-inline">
-                                    <input className="uk-input uk-border-pill" placeholder="Password" type="text" required/>
+                            <div className="login-margin">
+                                <div className="login-inline">
+                                    <input className="login-input login-border-pill" placeholder="Password" type="text" required/>
                                 </div>
                             </div>
 
-                            <div className="uk-margin uk-text-left padding-h uk-text-small  uk-preserve-color">
+                            <div className="login-margin login-text-left login-text-small">
                                 <label>
                                     Bạn là: 
-                                    <input className="uk-radio margin" type="radio" name="typeUser" required />Chủ trọ
-                                    <input className="uk-radio margin" type="radio" name="typeUser" required />Người thuê trọ
+                                    <input className="login-radio margin" type="radio" name="typeUser" required />Chủ trọ
+                                    <input className="login-radio margin" type="radio" name="typeUser" required />Người thuê trọ
                                 </label>
                             </div>
 
-                            <div className="uk-margin uk-text-left padding-h uk-text-small  uk-preserve-color">
-                                <label><input className="uk-checkbox" type="checkbox"/> Nhớ mật khẩu</label>
+                            <div className="login-margin login-text-left login-text-small">
+                                <label><input className="login-checkbox" type="checkbox"/> Nhớ mật khẩu</label>
                             </div>
 
-                            <button className="uk-button uk-width-1-1 uk-border-pill">Đăng nhập</button>
+                            <button className="login-button login-border-pill">Đăng nhập</button>
 
-                            <div className="uk-margin uk-width-1-1 uk-text-small">
-                            Bạn chưa có tài khoản? <NavLink className="ev-link-primary uk-text-bold" activeStyle={{color:'#fff'}} to="/signup" onClick = {this.props.changeState}>Đăng ký</NavLink>
-                            <a className="uk-margin uk-display-block" href="forgot-2.html*">Quên mật khẩu?</a>
+                            <div className="login-margin login-text-small">
+                            Bạn chưa có tài khoản? <NavLink className="login-link-primary login-text-bold" activeStyle={{color:'#fff'}} to="/signup" onClick = {this.props.changeState}>Đăng ký</NavLink>
+                            <a className="login-margin login-display-block" href="forgot-2.html*">Quên mật khẩu?</a>
                             </div>
                         </form>
                     </div>
