a249465b20cb80b98a7ae4936a35df5887dee53b
doanvanthanh3105 <bluedragon3105@gmail.com>
2020-12-01 20:16:54 +0700

    Listing

--- accommodationfinder/src/App.js
+++ accommodationfinder/src/App.js
@@ -1,43 +1,45 @@
-import React, { Component } from "react";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Post from "./components/Post";
+import React, { Component } from 'react';
+import Home from './components/Home';
+import Login from './components/Login';
+import Post from './components/Post';
 // import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Profile from './components/Profile';
-
+import Listing from './components/Listing';
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
+    }
 
-  }
-
-  render() {
-    return (
-      // this.renderedScreen(this.state.screen)
-      <Router>
-          <Navbar />
-          {/* A <Switch> looks through its children <Route>s and
+    render() {
+        return (
+            // this.renderedScreen(this.state.screen)
+            <Router>
+                <Navbar />
+                {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/post">
-              <Post />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-          </Switch>
-      </Router>
-    );
-  }
+                <Switch>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                    <Route path='/post'>
+                        <Post />
+                    </Route>
+                    <Route path='/profile'>
+                        <Profile />
+                    </Route>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
+                    <Route path='/listing'>
+                        <Listing />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
