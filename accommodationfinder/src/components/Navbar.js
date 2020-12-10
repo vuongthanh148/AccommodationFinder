f879e73e7b5284f6be5ffb4b68de5c3ccf8b148c
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-12-10 11:10:33 +0700

    Update navbar

--- accommodationfinder/src/components/Navbar.js
+++ accommodationfinder/src/components/Navbar.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
-// import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-// import { faFacebookF, faTwitter, faYoutube, faInstagram} from '@fortawesome/free-brands-svg-icons';
 import logo from '../image/logo_ngang_trang.png';
-// import { FaPhone } from 'react-icons/fa';
 import NavbarMobile from './NavbarMobile';
 import "../css/Navbar.css";
+import avatar from "../image/avatar.png";
+import { Icon, InlineIcon } from '@iconify/react';
+import chevronDown from '@iconify-icons/fa-solid/chevron-down';
+import chevronUp from '@iconify-icons/fa-solid/chevron-up';
+import envelope from '@iconify-icons/fa-solid/envelope';
+import bell from '@iconify-icons/fa-solid/bell';
+import logo_den from "../image/logo_ngang.png";
 
 class Navbar extends Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+      loggedIn: true
+    })
+  }
   render() {
     return (
       <div>
         <div className="display-nav">
-          <Bar />
+          <Bar loggedIn={this.state.loggedIn}/>
         </div>       
         <div className="display-moblie-nav">
           <NavbarMobile />
@@ -25,6 +35,29 @@ class Navbar extends Component {
 export default Navbar;
 
 class Bar extends Component {
+  constructor(props){
+    super(props)
+    this.state = ({
+      icon: chevronDown,
+      divAppearance: "none",
+    })
+  }
+  changeState = () => {
+    console.log("click");
+    if(this.state.divAppearance === "none") {
+      this.setState({
+        divAppearance: "block",
+        icon:chevronUp
+      })
+    }
+    else{
+      this.setState({
+        divAppearance: "none",
+        icon:chevronDown
+      })
+    }     
+  }
+
   render(){
     return(
       <div className="ev-header-1 navbar-section-primary">
@@ -33,7 +66,7 @@ class Bar extends Component {
           <div className="navbar-left">
             <NavLink className="navbar-logo" activeStyle={{color:'#fff'}} to="/home"><img className="ev-logo" src={logo}/></NavLink>
           </div>
-          <div className="navbar-right">
+          {this.props.loggedIn === false && <div className="navbar-right">
             <ul className="navbar-subnav navbar-subnav-divider navbar-right-element">
               <li>
                 <NavLink  activeStyle={{color:'#fff'}} to="/home">Trang chủ</NavLink>
@@ -45,7 +78,36 @@ class Bar extends Component {
               <NavLink  activeStyle={{color:'#fff'}} to="/signup" onClick = {this.props.changeState}>Đăng ký</NavLink>
               </li>
             </ul>
-          </div>
+          </div>}
+          {this.props.loggedIn === true && <div className="navbar1-right">
+            <ul className="navbar1-subnav navbar1-subnav-divider navbar1-right-element">
+              <li>
+                <NavLink  activeStyle={{color:'#fff'}} to="/home">Trang chủ</NavLink>
+              </li>
+              <li className="user-button">
+                
+                <a href="#" className="click-button" onClick={this.changeState}>
+                  <img className="navbar1-avatar-user" src={avatar}/>
+                  <div className="navbar1-user-name">Le Thi Hanh</div> 
+                  <span className="user-icon"><Icon icon={this.state.icon} /></span>
+                  
+                </a>           
+                <div className="user-navbar-dropdown" style={{display:this.state.divAppearance}}>
+                  <ul className="user-nav user-navbar-dropdown-nav">
+                    <li><NavLink  activeStyle={{color:'#fff'}} to="/profile" onClick={this.changeState}>Profile</NavLink></li>
+                    <li className="user-nav-divider"></li>
+                    <li onClick={this.changeState}><a>Dang xuat</a></li>
+                  </ul>
+                </div>               
+              </li>
+            </ul>
+            <a>
+              <span className="icon-padding"><Icon icon={envelope} className="icon-messeger"/></span>
+            </a>
+            <a>
+              <span className="icon-padding"><Icon icon={bell} className="icon-notification"/></span>
+            </a>
+          </div>}
         </nav>
     </div>
   )
