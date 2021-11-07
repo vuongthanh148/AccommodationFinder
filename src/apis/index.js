c66d09ea4c925f3e23fa7f775e24709f7cc84d08
Kenji <vuongthanh148@gmail.com>
2021-11-07 15:04:20 +0700

    change backend url

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-// export const BackendUrl = "https://accommodation-finder.herokuapp.com"
+export const BackendUrl = "https://accommodation-finder.herokuapp.com"
 export const ChatUrl = "https://accommodation-chat.herokuapp.com"
 // export const ChatUrl = "http://localhost:3002"
-export const BackendUrl = 'http://localhost:4000'
+// export const BackendUrl = 'http://localhost:4000'
 
 export const userToken = localStorage.getItem('token')
 
